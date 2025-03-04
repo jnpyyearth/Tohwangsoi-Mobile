c2d4e1683d42b17026c5be38960fd4f598427410
poppy <poppypopcorn@gmail.com>
2025-03-04 23:22:51 +0700

    connect firebase

--- app/src/main/java/com/example/tohwangsoi_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tohwangsoi_mobile/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.tohwangsoi_mobile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Firestore", "🛠️ Checking Firestore Connection...111")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +20,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        checkFirestoreConnection()
     }
+    private fun checkFirestoreConnection() {
+        Log.d("Firestore", "🛠️ Checking Firestore Connection...")
+
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("test_connection").document("dummy").get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    val status = document.getString("status") ?: "No status field"
+                    Log.d("Firestore11", "✅ Firestore connected successfully! Status: $status")
+                } else {
+                    Log.e("Firestore", "⚠️ Document does not exist!")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("Firestore", "❌ Firestore connection failed", e)
+            }
+    }
+
+
 }
